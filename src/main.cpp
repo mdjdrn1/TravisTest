00143ea71bdb15c676eacee91375d615e0ef3512
mdjdrn1 <mdjdrn1@gmail.com>
2017-10-03 18:20:13 +0200

    Fixed boost config in CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <string>
 #include <boost/filesystem.hpp>
+#include <any>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, " << std::endl;
 
     boost::filesystem::ofstream f("smile");
+    f << "Smile :)";
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "World!" << std::endl;
+
+    std::any a {13};
+
+    try {
+        std::cout << std::any_cast<int>(a) << std::endl;
+    }
+    catch(std::bad_any_cast) {
+        std::cout << "cannot cast variable \"a\" to int" << std::endl;
+    }
+
+    try {
+        std::cout << std::any_cast<std::string>(a) << std::endl;
+    }
+    catch(std::bad_any_cast) {
+        std::cout << "cannot cast variable \"a\" to string" << std::endl;
+    }
 
     return 0;
 }
