fca8543d216c79225fe3bd5409f72fe0872b430d
mdjdrn1 <mdjdrn1@gmail.com>
2017-10-03 18:07:46 +0200

    Fixed boost config in CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    boost::filesystem::ofstream f("smile");
+
+    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
