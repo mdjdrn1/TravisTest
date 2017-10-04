ec4e10dd8d7da2bfba39ebeca70ed518c6349277
mdjdrn1 <mdjdrn1@gmail.com>
2017-10-04 22:25:03 +0200

    Bit of cmake changes

--- test/basic.cpp
+++ test/basic.cpp
@@ -1,15 +1,19 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 int add(int a, int b) {
     return a + b;
 }
 
-TEST(testMath, myCubeTest) {
-    EXPECT_EQ(13, add(3, 10));
+TEST(MathTest, TwoPlusTwoEqualsFour) {
+    EXPECT_EQ(2 + 2, 4);
 }
 
+TEST(MathTest, Add) {
+    EXPECT_EQ(4, add(2, 2));
+    EXPECT_EQ(6, add(2, 4));
+}
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest( &argc, argv );
     return RUN_ALL_TESTS();
 }
